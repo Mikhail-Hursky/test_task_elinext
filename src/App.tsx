89a894ba53c1c6ff16aeaa0f8ee1c802a3b7e8fd
Mikhail Hursky <kakai747o@gmail.com>
2021-04-25 22:20:42 +0300

    feat: add activity mask

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "antd";
+import { debounce } from "lodash";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/header/Header";
 import Navigation from "./components/navigation/Navigation";
 import Footer from "./components/footer/Footer";
 import Content from "./components/content/Content";
+import Mask from "./components/mask/Mask";
 
 function App() {
+  const [isOff, setOff] = useState(false);
+
+  const mouseEvent = debounce((e: any) => {
+    console.log("X " + e.screenX, "Y " + e.screenY);
+    setOff(true);
+  }, 1000 * 60);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", mouseEvent);
+    return () => {
+      window.removeEventListener("mousemove", mouseEvent);
+    };
+  }, [isOff]);
+
   return (
     <>
-      <Router>
-        <Layout className="App">
-          <Header />
-          <Layout hasSider={true}>
-            <Navigation />
-            <Content />
+      {isOff ? (
+        <Mask setOff={setOff} />
+      ) : (
+        <Router>
+          <Layout className="App">
+            <Header />
+            <Layout hasSider={true}>
+              <Navigation />
+              <Content />
+            </Layout>
+            <Footer />
           </Layout>
-          <Footer />
-        </Layout>
-      </Router>
+        </Router>
+      )}
     </>
   );
 }
---
+++ src/components/mask/Mask.tsx
@@ -0,0 +1,26 @@
+import { Button, Result } from "antd";
+import React from "react";
+
+interface Props {
+  setOff(isOff: boolean): void;
+}
+
+export default function Mask({ setOff }: Props) {
+  return (
+    <>
+      <Result
+        title="You have not been active for a long time, so we ended your session."
+        extra={
+          <Button
+            onClick={() => {
+              setOff(false);
+            }}
+            type="primary"
+          >
+            Go Console
+          </Button>
+        }
+      />
+    </>
+  );
+}
