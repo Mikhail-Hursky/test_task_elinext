1fe5239be6301da15e4f384b45f6c95e1214491a
Mikhail Hursky <kakai747o@gmail.com>
2021-04-27 00:10:18 +0300

    feat: add requests my API

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { Photo } from "../interfaces/IPhoto";
 
 const URL = "https://www.flickr.com/services/rest/";
+const URL_MY_SERVER = "https://test-task-elinext.herokuapp.com/";
 const api_key = "be7600b3bff8c44546825c9906aeedb8";
 const format = "json";
 
@@ -24,6 +26,69 @@ export const Api = {
         return data.photos;
       })
       .catch((e) => console.log(e));
+    return res;
+  },
+
+  registration: async (name: string, password: string, email: string) => {
+    const res = await axios
+      .post(URL_MY_SERVER + "auth/registration", { name, password, email })
+      .then((response: any) => {
+        return response.message;
+      })
+      .catch((e) => e.message);
+    return res;
+  },
+
+  authorization: async (password: string, email: string) => {
+    const res = await axios
+      .post(URL_MY_SERVER + "auth/login", {
+        password,
+        email,
+      })
+      .then((response: any) => {
+        return { name: response.name, token: response.token };
+      })
+      .catch((e) => e.message);
+    return res;
+  },
+
+  deleteUserPhotoBySecretKey: async (token: string, secret: string) => {
+    const res = await axios
+      .delete(URL_MY_SERVER + `users/images?secret=${secret}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => ({ status: 200 }))
+      .catch((e) => e.message);
+    return res;
+  },
+
+  addUserPhoto: async (token: string, photo: Photo) => {
+    const res = await axios
+      .put(URL_MY_SERVER + "users/images", photo, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response: any) => {
+        return response.images;
+      })
+      .catch((e) => e.message);
+    return res;
+  },
+
+  getAllUserPhoto: async (token: string) => {
+    const res = await axios
+      .get(URL_MY_SERVER + "users/images", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response: any) => {
+        return response.images;
+      })
+      .catch((e) => e.message);
     return res;
   },
 };
